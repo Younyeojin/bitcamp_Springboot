ee9f367dcd9c78c6fe252f6c8c278c4eb802d58e
Younyeojin <yjyoon2107@gmail.com>
2021-07-07 20:01:29 +0900

    210707

--- src/main/java/com/example/demo/bank/controller/BankAccountController.java
+++ src/main/java/com/example/demo/bank/controller/BankAccountController.java
@@ -1,21 +1,28 @@
 package com.example.demo.bank.controller;
 
+import com.example.demo.bank.BankAccount;
 import com.example.demo.bank.controller.BankAccountController;
-import com.example.demo.bank.domain.BankAccount;
+
+
+import java.util.Scanner;
 
 public class BankAccountController {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //두개의 인스턴스 생성
         BankAccount yoon = new BankAccount();
         BankAccount park = new BankAccount();
-        //각 인스턴스를 대상으로 예금을 진행
-        yoon.deposit(5000);
-        park.deposit(3000);
-        //각 인스턴스를 대상으로 출금을 진행
-        yoon.withdraw(2000);
-        park.withdraw(2000);
-        //각 인스턴스를 대상으로 잔액을 조회
-        yoon.checkMyBalance();
-        park.checkMyBalance();
+        System.out.println("얼마를 입금하시겠습니까?");
+        yoon.setAmount(scanner.nextInt());
+        park.setAmount(scanner.nextInt());
+        yoon.deposit(yoon.getAmount());
+        park.deposit(park.getAmount());
+        System.out.println("얼마를 출금하시겠습니까?");
+        yoon.setAmount(scanner.nextInt());
+        park.setAmount(scanner.nextInt());
+        yoon.withdraw(yoon.getAmount());
+        park.withdraw(park.getAmount());
+        System.out.print(yoon.toString());
+        System.out.print(park.toString());
     }
 }
